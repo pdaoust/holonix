bbda58ad77d575e67d0ff69c5dd3e6b3c3f21a94
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-08-31 11:36:37 +0000

    default.nix -> default-legacy.nix, add holo-nixpkgs wrapper

---
+++ default-legacy.nix
@@ -0,0 +1,57 @@
+# This is the default nix file FOR HOLONIX
+# This file is what nix will find when hitting this repo as a tarball
+# This means that downstream consumers should pkgs.callPackage this file
+# See example.default.nix for an example of how to consume this file downstream
+{
+ # allow consumers to pass in their own config
+ # fallback to empty sets
+ config ? import ./config.nix
+}:
+let
+ pkgs = import ./nixpkgs;
+
+ app-spec-cluster = pkgs.callPackage ./app-spec-cluster { };
+ darwin = pkgs.callPackage ./darwin { };
+ rust = pkgs.callPackage ./rust { };
+ git = pkgs.callPackage ./git { };
+ dist = pkgs.callPackage ./dist {
+  rust = rust;
+  git = git;
+ };
+ n3h = pkgs.callPackage ./n3h { };
+ qt = pkgs.callPackage ./qt { };
+ release = pkgs.callPackage ./release {
+  config = config;
+ };
+ test = pkgs.callPackage ./test {
+   pkgs = pkgs;
+ };
+
+ holonix-shell = pkgs.callPackage ./nix-shell {
+  pkgs = pkgs;
+  app-spec-cluster = app-spec-cluster;
+  darwin = darwin;
+  dist = dist;
+  git = git;
+  n3h = n3h;
+  node = node;
+  openssl = openssl;
+  qt = qt;
+  release = release;
+  rust = rust;
+  test = test;
+ };
+
+ # override and overrideDerivation cannot be handled by mkDerivation
+ derivation-safe-holonix-shell = (removeAttrs holonix-shell ["override" "overrideDerivation"]);
+in
+{
+ # export the set used to build shell alongside the main derivation
+ # downstream devs can extend/override the shell as needed
+ # holonix-shell provides canonical dev shell for generic work
+ shell = derivation-safe-holonix-shell;
+ main = pkgs.stdenv.mkDerivation derivation-safe-holonix-shell;
+
+ # expose other things
+ rust = rust;
+}
