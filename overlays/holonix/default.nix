bdbf7b885b1d4485d9820f71d369b86c7e9f4bb4
samrose <samuel.rose@gmail.com>
2019-09-10 14:59:40 -0400

    release cut

--- overlays/holonix/default.nix
+++ overlays/holonix/default.nix
@@ -168,4 +168,27 @@ with final;
       exit 1;
     fi
   '';
+
+  hn-release-cut = writeShellScriptBin "hn-release-cut" ''
+    set -euo pipefail
+
+    echo "** START PREFLIGHT HOOK **"
+    ${config.release.hook.preflight}
+    echo "** END PREFLIGHT HOOK **"
+
+    hn-release-branch
+    hn-release-changelog
+
+    echo "** START VERSION HOOK **"
+    ${config.release.hook.version}
+    echo "** END VERSION HOOK **"
+
+    hn-release-push
+    hn-release-github
+
+    echo "** START PUBLISH HOOK **"
+    ${config.release.hook.publish}
+    echo "** END PUBLISH HOOK **"
+ '';
+
 }
